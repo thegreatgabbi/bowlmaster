3d2b8424e7ec2d7ec5d3d1b54c37bd33d11b80ee
thegreatgabbi <onethousand.words.88@gmail.com>
2018-08-21 15:36:47 +0800

    Completed: 232. 3D Sphere As Bowling Ball
    - Create a 3D sphere of the right size
    - Apply a temporary texture so we can see it roll
    - Add a sphere collider
    - Add a Rigidbody (3D) and set properties
    - Make the ball roll down the lane

---
+++ Assets/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+	public float launchSpeed = 10f;
+
+    private Rigidbody rigidbody;
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+
+        Launch();
+    }
+
+    private void Launch()
+    {
+        rigidbody.velocity = new Vector3(0, 0, launchSpeed);
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+	void Update () {
+		
+	}
+}
