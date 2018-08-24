cb418a47f0cc272989d7dfc950a651410f3b1192
thegreatgabbi <onethousand.words.88@gmail.com>
2018-08-24 11:19:53 +0800

    Completed: 217. Finishing Our Control Code
    - Go through Red > Green > Refactor loop strictly
    - Focus your failing tests on edge cases
    - Keep your ActionMaster.cs looking clean
    - Stop when you're confident it's done
    - Challenge the community to write a failing test

--- Assets/Editor/ActionMasterTest.cs
+++ Assets/Editor/ActionMasterTest.cs
@@ -4,35 +4,84 @@ using NUnit.Framework;
 using UnityEngine;
 
 [TestFixture]
-public class ActionMasterTest {
+public class ActionMasterTest
+{
 
     private ActionMaster actionMaster;
     private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+    private ActionMaster.Action endGame = ActionMaster.Action.EndGame;
     private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
+    private ActionMaster.Action reset = ActionMaster.Action.Reset;
 
     [SetUp]
-    public void Setup() {
+    public void Setup()
+    {
         actionMaster = new ActionMaster();
     }
 
     [Test]
-    public void T00PassingTest() {
+    public void T00PassingTest()
+    {
         Assert.AreEqual(1, 1);
     }
 
     [Test]
-    public void T01OneStrikeReturnsEndTurn() {
+    public void T01OneStrikeReturnsEndTurn()
+    {
         Assert.AreEqual(endTurn, actionMaster.Bowl(10));
     }
 
     [Test]
-    public void T02Bowl8ReturnsTidy() {
+    public void T02Bowl8ReturnsTidy()
+    {
         Assert.AreEqual(tidy, actionMaster.Bowl(8));
     }
 
     [Test]
-    public void T03RemainingPinsReturnsEndTurn() {
+    public void T03RemainingPinsReturnsEndTurn()
+    {
         actionMaster.Bowl(2);
         Assert.AreEqual(endTurn, actionMaster.Bowl(7));
     }
+
+    [Test]
+    public void T04CheckResetAtStrikeInLastFrame()
+    {
+        for (int i = 1; i <= 18; i++) // bowl 18 times
+        {
+            actionMaster.Bowl(1);
+        }
+        Assert.AreEqual(reset, actionMaster.Bowl(10));
+    }
+
+    [Test]
+    public void T05CheckResetAtSpareInLastFrame()
+    {
+        for (int i = 1; i <= 19; i++) // bowl 19 times
+        {
+            actionMaster.Bowl(1);
+        }
+        Assert.AreEqual(reset, actionMaster.Bowl(9));
+    }
+
+    [Test]
+    public void T06Bowl21NotAwardedInLastFrame()
+    {
+        for (int i = 1; i <= 19; i++) // bowl 19 times
+        {
+            actionMaster.Bowl(1);
+        }
+        Assert.AreEqual(endGame, actionMaster.Bowl(1));
+    }
+
+    [Test]
+    public void T07GameEndsAtBowl21() {
+        for (int i = 1; i <= 19; i++) // bowl 19 times
+        {
+            actionMaster.Bowl(1);
+        }
+        actionMaster.Bowl(9); // bowl 20
+        Assert.AreEqual(endGame, actionMaster.Bowl(10)); // bowl 21
+    }
+
 }
--- Assets/Scripts/ActionMaster.cs
+++ Assets/Scripts/ActionMaster.cs
@@ -4,14 +4,33 @@ using UnityEngine;
 
 public class ActionMaster {
 
+    // Reset just means put back the pins
+    // EndTurn will change the player's turn (and presumably Reset as well)
     public enum Action {Tidy, Reset, EndTurn, EndGame}
 
     private int bowl = 1;
+    private int[] bowls = new int[21];
 
     public Action Bowl(int pins) {
 
         if (pins < 0 || pins > 10) {
             throw new UnityException("Invalid pins");
+        }
+
+        // register the current bowl
+        bowls[bowl - 1] = pins;
+
+        if (bowl == 21) {
+            return Action.EndGame;
+        }
+
+        if (bowl >= 19 && Bowl21Awarded()) {
+            bowl += 1;
+            return Action.Reset;
+        }
+
+        if (bowl == 20 && !Bowl21Awarded()) {
+            return Action.EndGame;
         }
 
         if (pins == 10) {
@@ -32,5 +51,9 @@ public class ActionMaster {
         // other actions here
 
         throw new UnityException("Not sure what action to return");
+    }
+
+    private bool Bowl21Awarded() {
+        return (bowls[19 - 1] + bowls[20 - 1] >= 10);
     }
 }
